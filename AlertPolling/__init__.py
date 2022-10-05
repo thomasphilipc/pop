eb7d8b2df11a376ae9b0f17f9957effd9eaa2497
TP <thomas.philip@vectorglobe.com>
2022-10-05 05:36:35 +0530

    sintorans2

---
+++ AlertPolling/__init__.py
@@ -0,0 +1,79 @@
+import datetime
+import logging
+from re import A
+import requests
+import json
+import azure.functions as func
+from datetime import timedelta
+import os
+
+
+def get_units(group_id,apikey):
+
+    total_units = []
+
+    url = "https://mapon.com/api/v1/unit_groups/list_units.json?key="+str(apikey)+"&id=" + str(group_id)
+    logging.info(url)
+    payload = ''
+    headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    responserecvd = json.loads(response.text.encode('utf8'))
+    logging.info(responserecvd)
+    list_list = responserecvd["data"]
+    unit_ids = list_list
+    if(unit_ids['units']):
+        for unit in unit_ids['units']:
+            total_units.append(unit['id'])
+    logging.info( " data  output in durable activityGetUnits is format %s",str(total_units))
+    return (total_units)
+
+
+def main(mytimer: func.TimerRequest, clientTable, msg : func.Out[str]) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    payload = json.loads(clientTable)
+
+    logging.info(payload)
+    #'[{"apiKey":"dasdsd4234","clientKey":"testkey4","clientName":"VectorGlobe Fe+","customField":"{\'Package Type\': {\'id\': 2544281, \'settings\': {\'options\': {\'6338f5d755f12\': \'type1\', \'6338f5d755f15\': \'type 2\', \'6338f5d755f16\': \'type 3\'}}}, \'Packing List Upload\': {\'id\': 2526197, \'settings\': []}, \'Tracking Link\': {\'id\': 2541055, \'settings\': []}}","enabled":true,"process_alerts":true,"total_units":"14","PartitionKey":"1209","RowKey":"58234"},{"apiKey":"511687b33edad08b83303995472b3106ec3dff13","clientKey":"testkey3","clientName":"SteadyRoutes","customField":"{\'Trailer Type\': {\'id\': 1492749, \'settings\': []}, \'Device ID\': {\'id\': 2353966, \'settings\': []}, \'Trailer Plate Number\': {\'id\': 2013009, \'settings\': []}, \'Trailer Reg Expiry\': {\'id\': 2013010, \'settings\': []}}","enabled":true,"process_alerts":false,"total_units":"34","PartitionKey":"24752","RowKey":"58572"},{"apiKey":"30484e21fef6179c8bfce893968c4fcd2ce29cc4","clientKey":"testkey2","clientName":"Huawei","customField":"{\'Vehicle Inspection Date\': {\'id\': 2404102, \'settings\': []}, \'Shipment ID\': {\'id\': 2323261, \'settings\': []}, \'Driver Name\': {\'id\': 2323262, \'settings\': []}, \'Driver Phone No.\': {\'id\': 2323263, \'settings\': []}, \'Carrier Name\': {\'id\': 2323264, \'settings\': []}, \'Business Type\': {\'id\': 2323265, \'settings\': {\'options\': {\'628dbbf3d88d2\': \'1: Hong Kong local Pickup\', \'628dbbf3d88d6\': \'2: China-Hong Kong transportation\', \'628dbbf3d88d7\': \'3: Revolving transportation of sorting centre\', \'628dbbf3d88d8\': \'4: Transportation in China\', \'630dab7541ab6\': \'8: DSC OUTBOUND\'}}}, \'Vehicle Style\': {\'id\': 2323266, \'settings\': {\'options\': {\'628dbbf3d8c53\': \'VS1: common rail truck\', \'628dbbf3d8c56\': \'VS2: common van\', \'628dbbf3d8c57\': \'VS3: container tractor\', \'628dbbf3d8c58\': \'VS4: flat truck\', \'628dbbf3d8c59\': \'VS5: closed truck\', \'628dbbf3d8c5a\': \'VS6: tank truck\', \'628dbbf3d8c5b\': \'VS7: Special structure truck\', \'628dbbf3d8c5d\': \'VS8: other types\', \'628dbbf3d8c5e\': \'VS9: tractor\', \'628dbbf3d8c5f\': \'VS10: semi-trailer-car cabinet\', \'628dbbf3d8c60\': \'VS11: semi-trailer-plate\', \'628dbbf3d8c61\': \'VS12: Semi-trailer-container\'}}}, \'Truck Registration Date\': {\'id\': 2323267, \'settings\': []}, \'Truck Registration Expiry\': {\'id\': 2323268, \'settings\': []}, \'LSP Code\': {\'id\': 2323269, \'settings\': []}, \'Vehicle Type\': {\'id\': 2323270, \'settings\': []}, \'Car Company\': {\'id\': 2323271, \'settings\': []}, \'Car Country Name\': {\'id\': 2323272, \'settings\': []}, \'Car Manager Name\': {\'id\': 2323273, \'settings\': []}, \'Car Manager Phone. No.\': {\'id\': 2323274, \'settings\': []}, \'License Plate (Other Aux)\': {\'id\': 2323275, \'settings\': []}, \'Bill ID\': {\'id\': 2323277, \'settings\': []}, \'Bill Type\': {\'id\': 2323278, \'settings\': []}}","enabled":true,"process_alerts":false,"total_units":"2","PartitionKey":"40466","RowKey":"60317"}]'
+
+    # the time to check back upto is given as config
+    pollduration = int(os.environ["pollduration"])
+    #details from payload
+    for item in payload:
+        if item["process_alerts"]:
+            group_id= item['RowKey']
+            apikey=item['apiKey']
+            units=get_units(group_id,apikey)
+            item['units']=units
+
+            #times
+            end = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat().split('.')[0]+'Z'
+            start = (datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)-timedelta(minutes=pollduration)).isoformat().split('.')[0]+'Z'
+
+            endpoint = "https://mapon.com/"
+            path = "api/v1/alert/list.json?key="+apikey
+            params="&from="+start+"&till="+end+"&include[]=address&include[]=location&limit=150&page=1"
+            constructed_url = endpoint + path + params
+
+            payload={}
+            headers = {}
+
+
+
+            response = requests.request("GET", constructed_url, headers=headers, data=payload)
+
+            alerts_json=response.json()
+
+            alert_list=alerts_json["data"]
+
+            for item in alert_list:
+                if item["unit_id"] in units:
+                    msg.set(json.dumps(item))
+
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
---
+++ AlertQueueTrigger/__init__.py
@@ -0,0 +1,109 @@
+import logging
+import json
+import azure.functions as func
+import os
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
+import smtplib 
+import ssl
+
+
+def send_mail(to_email,message, from_email='helpdesk@vectorglobe.com'):
+    # import smtplib
+    email_password = "BE3/FwGJncmCHD6jGJK+yO72PhtMUyxXgbBqwMujOxqM"
+    email_username = "AKIAVDC7VR5F4WH7RP7E"
+    context = ssl.create_default_context()
+    port=587
+    req_message = message.encode('utf-8')
+
+    smtp_server="email-smtp.us-west-2.amazonaws.com"
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls(context=context)
+        server.login(email_username, email_password)
+        server.sendmail(from_email, to_email, req_message)
+
+
+
+def main(msg: func.QueueMessage) -> None:
+    logging.info('Python queue trigger function processed a queue item: %s',
+                 msg.get_body().decode('utf-8'))
+
+  #{
+  #"unit_id": 101545,
+  #"time": "2022-10-04T01:39:55Z",
+  #"alert_type": "speeding",
+  #"alert_val": "{\"source\":\"manual\",\"speed\":82,\"speed_limit\":80,\"overspeed_limit\":0}",
+  #"msg": "Driving speed is 82 km/h (Manual speed limit: 80 km/h)",  
+  #"address": "Līči, Krapes pag., Ogres nov., Latvija",
+  #"location": "56.71888,25.20018"
+  #}
+
+    data=json.dumps(msg.get_body().decode('utf-8'))
+    json_data=json.loads(json.loads(data))
+
+    logging.info(json_data)
+
+    tableStorageKey = os.environ["tableStorageKey"]
+    assettableName = os.environ["tableName"]
+    accountName = os.environ["accountName"] 
+    customfieldTableName = os.environ["customfieldTableName"]
+    unit_id=str(json_data["unit_id"])
+    table_service = TableService(account_name=accountName, account_key=tableStorageKey)
+    query="RowKey eq'"+str(unit_id)+"'"
+    assets = table_service.query_entities(assettableName, filter=query)
+    asset_det =json.dumps(assets, indent=4,sort_keys=True, default=str )
+    assetsc = table_service.get_entity(customfieldTableName, row_key=unit_id, partition_key="customfield")
+    for item in assets:
+        asset_det_s = json.dumps(item, indent=4,sort_keys=True, default=str )
+    asset_det=json.loads(asset_det_s)
+    assetscf=json.loads(json.dumps(assetsc, indent=4,sort_keys=True, default=str))
+    logging.info(asset_det)
+    logging.info(assetscf)
+    goods_det = json.loads(assetscf.get("AdditionalField",None))
+    cf_det = json.loads(assetscf.get("CustomField",None))
+    #{
+    #"PartitionKey": "8180",
+    #"RowKey": "50043",
+    #"Timestamp": "2022-10-03 00:37:33.835730+00:00",
+    #"assetName": "SM-55",
+    #"boxId": "207568",
+    #"clientName": "VectorGlobe Fe+",
+    #"country_code": "RU",
+    #"created_at": "2016-09-13T10:00:15Z",
+    #"direction": 184,
+    #"etag": "W/\"datetime'2022-10-03T00%3A37%3A33.8357302Z'\"",
+    #"imei": "358679068009470",
+    #"label": "Mercedz Benz",
+    #"last_update": "2022-10-03T00:37:15Z",
+    #"lat": 56.0036,
+    #"lon": 37.18986
+    #}
+    #{'PartitionKey': 'customfield', 'RowKey': '50043', 'Timestamp': datetime.datetime(2022, 10, 2, 6, 1, 17, 429404, tzinfo=tzutc()), 'CustomField': '{"PackageType": null, "PackingListUpload": null, "TrackingLink": null}', 'etag': 'W/"datetime\'2022-10-02T06%3A01%3A17.4294046Z\'"'}
+    required_payload = {
+        "vehicle_reg" : asset_det["assetName"],
+        "route" : asset_det["label"],
+        "group" : asset_det["clientName"],
+        "htm" : goods_det["htm"],
+        "packing_list" : goods_det["packing_list"],
+        "tracking_link" : cf_det["TrackingLink"],
+        "alert_name" : json_data["alert_type"],
+        "location" : json_data["location"],
+        "geo_reference" : json_data["address"],
+        "alert_comment" : json_data["msg"],
+        "time" : json_data["time"]
+    }
+    logging.info(required_payload)
+    #{'vehicle_reg': 'FM920 - Beacons testing', 'route': 'VectorGlobe Fe+', 'htm': 'HTm123123123123', 'packing_list': ['pl1231231', 'pl54134123', 'pl15123123', 'pl5123132123'], 'tracking_link': 'http://please.com', 'alert_name': 'speeding', 'location': '56.71888,25.20018', 'geo_reference': 'Lici, Krapes pag., Ogres nov., Latvija', 'alert_comment': 'Driving speed is 82 km/h (Manual speed limit: 80 km/h)'}
+
+    req_subject = required_payload["vehicle_reg"] + " on route " + required_payload.get("group", "Not Available") + "/" + required_payload["route"] + " has triggered the " + required_payload["alert_name"] + " alert."
+
+    req_message ="Subject: "+req_subject+"\n\n"
+    req_message += "Hi Customer," + "\n"
+    req_message += req_subject + "\n"
+    req_message += "Alert triggered at location coordinate " + required_payload["location"] + " which is " + required_payload["geo_reference"] + " on " + required_payload["time"] + " and the reason was " + required_payload["alert_comment"] + "\n"
+    req_message += "You can access the live location using the tracking link " + required_payload["tracking_link"] + "\n\n"
+    req_message += "The additional details for this are " + "\n"
+    req_message += "HTM Number :" + required_payload["htm"] + "\n"
+    for item in required_payload["packing_list"]:
+        req_message += item + "\n"
+    send_mail(to_email="helpdesk@vectorglobe.com", message=req_message)
